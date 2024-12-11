4252bb0f48ba1221fbb7f5073896aa933b9c7645
achaa <hafshahazzahraa@gmail.com>
2024-12-11 12:00:40 +0700

    test1

---
+++ Test/TodoListViewTest.php
@@ -0,0 +1,60 @@
+<?php
+
+require_once __DIR__ . '/../Entity/TodoList.php';
+require_once __DIR__ . '/../Repository/TodoListRepository.php';
+require_once __DIR__ . '/../Service/TodoListService.php';
+require_once __DIR__ . '/../View/TodoListView.php';
+require_once __DIR__ . '/../Helper/TodoListHelper.php';
+
+use \Entity\TodoList;
+use \Repository\TodoListRepositoryImpl;
+use \Service\TodoListServiceImpl;
+use \View\TodoListView;
+
+function testViewShowTodoList(): void
+{
+
+    $todoListRepository = new TodoListRepositoryImpl();
+    $todoListService = new TodoListServiceImpl($todoListRepository);
+    $todoListView = new TodoListView($todoListService);
+
+    $todoListService->addTodoList("BELAJAR PHP");
+    $todoListService->addTodoList("BELAJAR PHP OOP");
+    $todoListService->addTodoList("BELAJAR PHP Database");
+
+    $todoListService->showTodoList();
+
+}
+
+function testViewAddTodoList(): void 
+{
+
+    $todoListRepository = new TodoListRepositoryImpl();
+    $todoListService = new TodoListServiceImpl($todoListRepository);
+    $todoListView = new TodoListView($todoListService);
+
+    $todoListService->addTodoList("Belajar PHP");
+    $todoListService->addTodoList("Belajar PHP OOP");
+    $todoListService->addTodoList("Belajar PHP Database");
+    $todoListService->showTodoList();
+    $todoListView->addTodoList();
+    $todoListService->showTodoList();
+    $todoListView->addTodoList();
+    $todoListService->showTodoList();
+}
+
+function testViewRemoveTodoList(): void
+{
+    $todoListRepository = new TodoListRepositoryImpl();
+    $todoListService = new TodoListServiceImpl($todoListRepository);
+    $todoListView = new TodoListView($todoListService);
+    $todoListService->addTodoList("BELAJAR PHP");
+    $todoListService->addTodoList("BELAJAR PHP OOP");
+    $todoListService->addTodoList("BELAJAR PHP Database");
+    $todoListService->showTodoList();
+    $todoListView->removeTodoList();
+    $todoListService->showTodoList();
+    $todoListView->removeTodoList();
+    $todoListService->showTodoList();
+}
+testViewRemoveTodoList(); 
