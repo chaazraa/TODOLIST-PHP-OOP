28f51d7f2f470e054d9263467087ae5b7b501b9e
achaa <hafshahazzahraa@gmail.com>
2024-12-11 11:04:19 +0700

    service

--- Service/TodoListService.php
+++ Service/TodoListService.php
@@ -12,11 +12,11 @@ namespace Service {
         public function __construct(TodoListRepository $todoListRepository) {
             $this->todoListRepository = TodoListRepository;
         }
-        function showTodoList(): void; {
+        function showTodoList(): void {
             echo "TODOLIST" . PHP_EOL;
             $todoList = $this->todoListRepository->findALL();
             foreach ($todoList as $number => $value) {
-                echi "$number. " . $value->getTodo() . PHP_EOL;
+                echo "$number. " . $value->getTodo() . PHP_EOL;
             }
         }
         function addTodoList(string $todo): void {
